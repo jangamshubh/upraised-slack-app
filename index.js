c76eec734c249a0cdbe8f6659c98f374c8b18794
Shubh Jangam <shubh.jangam@chitran.in>
2021-08-20 00:52:01 +0530

    v1.2 - Timings Updated as given in the PDF

--- index.js
+++ index.js
@@ -28,46 +28,109 @@ app.get("/", (req, res) => {
 });
 
 // Monday Create  Conversation
-cron.schedule('05 23 * * 4', function() {
+cron.schedule('52 14 * * 1', function() {
     createConversations.createChannels();
     console.log("Channel Creation Successful - Monday")
 });
 // Monday Update Conversation
-cron.schedule('07 23 * * 4', function() {
+cron.schedule('55 14 * * 1', function() {
     manageConversations.getAllChannels();
     console.log("Channel Updation Successful - Monday")
 });
 // Monday Create Or Update Messages
-cron.schedule('09 23 * * 4', function() {
+cron.schedule('58 14 * * 1', function() {
     addMessagesToDatabase.getAllMessages();
     console.log("Updation or Creation of Messages - Monday")
 });
 // Monday Send Messages
-cron.schedule('11 23 * * 4', function() {
+cron.schedule('00 15 * * 1', function() {
     sendMessagesToSlack.PodcastMessages();
     console.log("Sending Messages Successful - Monday")
 });
 
+// Tuesday Create Conversation
+cron.schedule('52 15 * * 2', function() {
+    createConversations.createChannels();
+    console.log("Channel Creation Successful - Tuesday")
+});
+// Tuesday Update Conversation
+cron.schedule('55 15 * * 2', function() {
+    manageConversations.getAllChannels();
+    console.log("Channel Updation Successful - Tuesday")
+});
+// Tuesday Create Or Update Messages
+cron.schedule('58 15 * * 2', function() {
+    addMessagesToDatabase.getAllMessages();
+    console.log("Updation or Creation of Messages - Tuesday")
+});
+// Tuesday Send Messages
+cron.schedule('00 16 * * 2', function() {
+    sendMessagesToSlack.ArticleMessages();
+    console.log("Sending Messages Successful - Tuesday")
+});
 
-// Thursday Create  Conversation
-cron.schedule('15 23 * * 4', function() {
+// Wednesday Create Conversation
+cron.schedule('52 16 * * 3', function() {
+    createConversations.createChannels();
+    console.log("Channel Creation Successful - Wednesday")
+});
+// Wednesday Update Conversation
+cron.schedule('55 16 * * 3', function() {
+    manageConversations.getAllChannels();
+    console.log("Channel Updation Successful - Wednesday")
+});
+// Wednesday Create Or Update Messages
+cron.schedule('58 16 * * 3', function() {
+    addMessagesToDatabase.getAllMessages();
+    console.log("Updation or Creation of Messages - Wednesday")
+});
+// Wednesday Send Messages
+cron.schedule('00 17 * * 3', function() {
+    sendMessagesToSlack.NewsLetterMessages();
+    console.log("Sending Messages Successful - Wednesday")
+});
+
+
+// Thursday Create Conversation
+cron.schedule('52 16 * * 4', function() {
     createConversations.createChannels();
     console.log("Channel Creation Successful - Thursday")
 });
 // Thursday Update Conversation
-cron.schedule('17 23 * * 4', function() {
+cron.schedule('55 16 * * 4', function() {
     manageConversations.getAllChannels();
     console.log("Channel Updation Successful - Thursday")
 });
 // Thursday Create Or Update Messages
-cron.schedule('19 23 * * 4', function() {
+cron.schedule('58 16 * * 4', function() {
     addMessagesToDatabase.getAllMessages();
     console.log("Updation or Creation of Messages - Thursday")
 });
 // Thursday Send Messages
-cron.schedule('21 23 * * 4', function() {
+cron.schedule('00 17 * * 4', function() {
     sendMessagesToSlack.PodcastMessages();
     console.log("Sending Messages Successful - Thursday")
+});
+
+// Friday Create Conversation
+cron.schedule('52 14 * * 5', function() {
+    createConversations.createChannels();
+    console.log("Channel Creation Successful - Friday")
+});
+// Friday Update Conversation
+cron.schedule('55 14 * * 5', function() {
+    manageConversations.getAllChannels();
+    console.log("Channel Updation Successful - Friday")
+});
+// Friday Create Or Update Messages
+cron.schedule('58 14 * * 5', function() {
+    addMessagesToDatabase.getAllMessages();
+    console.log("Updation or Creation of Messages - Friday")
+});
+// Friday Send Messages
+cron.schedule('00 15 * * 5', function() {
+    sendMessagesToSlack.BookMessages();
+    console.log("Sending Messages Successful - Friday")
 });
 
 
