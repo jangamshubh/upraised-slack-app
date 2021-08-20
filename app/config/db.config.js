e10bb26b4be4ff5178c5ade577e6075081d55eec
Shubh Jangam <shubh.jangam@chitran.in>
2021-08-20 11:32:29 +0530

    some bug fixing for database config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,6 +4,8 @@ module.exports = {
     PASSWORD: "41a8b247655bae6d6c4e81f6530afcdd583949d0819e9d2217be6a54235228b8",
     DB: "d5oi2blasuobi3",
     dialect: "postgres",
+    PORT: "5432",
+    SSL:true,
     pool: {
         max: 5,
         min: 0,
--- app/models/index.js
+++ app/models/index.js
@@ -5,7 +5,6 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
     operatorsAliases: false,
-
     pool: {
         max: dbConfig.pool.max,
         min: dbConfig.pool.min,
