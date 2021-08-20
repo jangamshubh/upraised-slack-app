f97b4e5aafef73bcca81f5540330483a550265f6
Shubh Jangam <shubh.jangam@chitran.in>
2021-08-20 10:47:33 +0530

    v1.3 - Added Env Example and changed db config for heroku

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "test",
-    DB: "upraised-slack-automessaging",
+    HOST: "ec2-52-3-130-181.compute-1.amazonaws.com",
+    USER: "cugnupexqgfgxm",
+    PASSWORD: "41a8b247655bae6d6c4e81f6530afcdd583949d0819e9d2217be6a54235228b8",
+    DB: "d5oi2blasuobi3",
     dialect: "postgres",
     pool: {
         max: 5,
